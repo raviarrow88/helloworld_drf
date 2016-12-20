f6778ed7cb964468e727b784ad994393ad5ebf13
Ravi Teja <raviarrow88@gmail.com>
2016-12-20 15:19:10 +0530

    API getting the list done

--- hello_drf/helloapi/views.py
+++ hello_drf/helloapi/views.py
@@ -3,9 +3,11 @@ from .models import Employee
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serailizer import EmployeeSerializer
+import json
 
 
 class EmployeeViewList(APIView):
-    def display(self):
-        data = '<html><body><h1>Hello, world</h1></body></html>'
-        return Response({'message': data})
+    def get(self,request):
+        employees = Employee.objects.all()
+        serailizer_class = EmployeeSerializer(employees,many=True)
+        return Response(serailizer_class.data)
